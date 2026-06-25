82c094a4c41403cf7021847bb4c6a24b6ca2dac5
shepz91 <shepzy23@googlemail.com>
2026-06-25 20:57:19 +0100

    Add files via upload

--- sticky_notes_project/sticky_notes/notes/views.py
+++ sticky_notes_project/sticky_notes/notes/views.py
@@ -2,19 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import StickyNote
 from .forms import StickyNoteForm
 
-"""View all notes and create the initial blank form"""
-
 
 def note_list(request):
+    """View all notes and create the initial blank form"""
+    
     notes = StickyNote.objects.all()
     form = StickyNoteForm()  # Empty form ready for creation
     return render(request, "notes/note_list.html", {"notes": notes, "form": form})
 
 
-"""Save a new sticky note"""
-
-
 def note_create(request):
+    """Save a new sticky note"""
+
     if request.method == "POST":
         form = StickyNoteForm(request.POST)
         if form.is_valid():
@@ -22,10 +21,8 @@ def note_create(request):
     return redirect("note_list")
 
 
-"""Edit an existing note"""
-
-
 def note_update(request, pk):
+    """Edit an existing note"""
     note = get_object_or_404(StickyNote, pk=pk)
 
     if request.method == "POST":
@@ -39,10 +36,8 @@ def note_update(request, pk):
     return render(request, "notes/note_edit.html", {"form": form, "note": note})
 
 
-"""Remove a note"""
-
-
 def note_delete(request, pk):
+    """Remove a note"""
     note = get_object_or_404(StickyNote, pk=pk)
     if request.method == "POST":
         note.delete()
